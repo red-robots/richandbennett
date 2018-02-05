2a3775b29f84d02d6bd03374669be51854b22ada
Austin Crane <austincrane@gmail.com>
2018-02-05 12:23:05 -0500

    added hovers

--- footer.php
+++ footer.php
@@ -14,7 +14,9 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<img class="blocks" src="<?php echo get_template_directory_uri()."/images/blocks.png";?>" alt="blocks">
+		<?php if( is_front_page() ) : ?>
+			<img class="blocks" src="<?php echo get_template_directory_uri()."/images/blocks.png";?>" alt="blocks">
+		<?php endif; ?>
 		<div class="wrapper cap">
 			<?php $mailing_line_1 = get_field("mailing_line_1","option");
 			$mailing_line_2 = get_field("mailing_line_2","option");
