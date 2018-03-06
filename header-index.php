e351cb3486692dc1d7032a7f7267a4d786ef47b8
Austin Crane <austincrane@gmail.com>
2018-03-06 11:03:23 -0500

    add tracking fb pixel

--- header-index.php
+++ header-index.php
@@ -17,7 +17,11 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-<?php wp_head(); ?>
+<?php 
+
+get_template_part('template-part/facebook');
+
+wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
