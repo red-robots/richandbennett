1154bab4de7deba3b26db27e4d58921576ec6938
Fritz <fre.healy@gmail.com>
2018-03-23 15:04:38 -0400

    changing internal header to match home

--- header.php
+++ header.php
@@ -70,31 +70,68 @@ if( is_front_page() ) { ?>
 				<div class="button">Menu</div><!--.button-->
 			</div><!--.button-wrapper-->
 			<div class="col-2">
-				<div class="box st-pats first">
-					<a href="<?php echo get_permalink(7);?>">
-						<?php include get_template_directory()."/images/icon-white-st-pats.svg";?>
-					</a>
-				</div><!--.box-->
-				<div class="box halloween">
-					<a href="<?php echo get_permalink(40);?>">
-						<?php include get_template_directory()."/images/icon-white-halloween.svg";?>
-					</a>
-				</div><!--.box-->
-				<div class="box americrawl">
-					<a href="<?php echo get_permalink(9);?>">
-						<?php include get_template_directory()."/images/icon-white-americrawl.svg";?>
-					</a>
-				</div><!--.box-->
-				<div class="box nye">
-					<a href="<?php echo get_permalink(38);?>">
-						<?php include get_template_directory()."/images/icon-white-nye.svg";?>
-					</a>
-				</div><!--.box-->
-				<div class="box tubing">
-					<a href="<?php echo get_permalink(11);?>">
-						<?php include get_template_directory()."/images/icon-white-tubing.svg";?>
-					</a>
-				</div><!--.box--> 
+				<?php $current_date = date('Ymd');
+				$events = array(7,40,9,38,11);
+				$dates = array();
+				foreach($events as $event):
+					$dates[intval(get_field("event_date",$event))?intval(get_field("event_date",$event)):22222222] = $event;
+				endforeach;
+				krsort($dates);
+				$correct_ordered_dates = array();
+				$index = 0;
+				foreach($dates as $date=>$id):
+					if($current_date<= $date):
+						array_unshift($correct_ordered_dates,$id);
+					else: 
+						$correct_ordered_dates = array_merge($correct_ordered_dates,array_reverse(array_slice($dates,$index)));
+						break;
+					endif;
+					$index++;
+				endforeach;
+				$args = array(
+					'post_type'=>'page',
+					'post__in'=>$correct_ordered_dates,
+					'orderby'=>'post__in'
+				);
+				$query = new WP_Query($args);
+				if($query->have_posts()):
+					$i = 0;?>
+					<?php while($query->have_posts()):$query->the_post();
+						$id = get_the_ID();
+						$image = null;
+						$name = null;
+						switch($id):
+							case 7:
+								$name = "st-pats";
+								$image = "/images/icon-white-st-pats.svg";
+								break;
+							case 40:
+								$name = "halloween";
+								$image = "/images/icon-white-halloween.svg";
+								break;
+							case 9:
+								$name = "americrawl";
+								$image = "/images/icon-white-americrawl.svg";
+								break;
+							case 38:
+								$name = "nye";
+								$image = "/images/icon-white-nye.svg";
+								break;
+							case 11:
+								$name = "tubing";
+								$image = "/images/icon-white-tubing.svg";
+								break;
+						endswitch;?>
+						<div class="box <?php echo $name;?> <?php if($i == 0) echo "first";?>">
+							<a href="<?php echo get_permalink($id);?>">
+								<?php include get_template_directory().$image;?>
+							</a>
+						</div><!--.box--> 
+						<?php $i++;
+					endwhile;?>
+				<?php $post = get_post(5);
+				setup_postdata($post);
+				endif;?>
 				<div class="box dots last">
 						<?php include get_template_directory()."/images/more-dots.svg";?>
 				</div><!--.box--> 
